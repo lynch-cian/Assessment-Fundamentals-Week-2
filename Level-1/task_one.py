4817f198b0d114b164de263c1a17f08a1ca73c45
Cian Lynch <cianlynch345@gmail.com>
2026-03-06 14:16:38 +0000

    Add Trainee class, get_age(), add_assessment(), and get_assessment()

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,6 +1,44 @@
 from datetime import date
 
 
+class Trainee():
+    def __init__(self, name: str, email: str, date_of_birth: date, assessments: list[Assessment]):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = assessments
+
+    def get_age(self) -> int:
+        today = date.today()
+        age = int(today.year) - self.date_of_birth.year - \
+            ((int(today.month), int(today.day)) <
+             (self.date_of_birth.month, self.date_of_birth.day))
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        self.assessments.append(assessment)
+        return None
+
+    def get_assessment(self, name: str) -> None:
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+        return None
+
+
+class Assessment():
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        allowed_types = ["multiple-choice", "technical", "presentation"]
+        if type in allowed_types:
+            self.type = type
+        else:
+            raise ValueError("Type must be in allowed types.")
+        if score >= 0 and score <= 100:
+            self.score = score
+        else:
+            raise ValueError("Score must be between 0-100")
+
 
 if __name__ == "__main__":
     trainee = Trainee("Sigma", "trainee@sigmalabs.co.uk", date(1990, 1, 1))
