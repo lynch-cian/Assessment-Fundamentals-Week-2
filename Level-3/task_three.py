c98b003871752475022a35259aea132ee53342d5
Cian Lynch <cianlynch345@gmail.com>
2026-03-06 15:45:59 +0000

    Level 3, added Marking class, mark(), and generate_assessment()

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -1,17 +1,119 @@
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 2 BELOW
-#
-#####
+from datetime import date
+
+allowed_types = ["multiple-choice", "technical", "presentation"]
 
 
+class Marking():
+    def __init__(self, quiz: Quiz, assessment: Assessment = None) -> None:
+        self._quiz = quiz
+        if assessment == None:
+            self.assessment = self.generate_assessment()
+        else:
+            self.assessment = assessment
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 2 ABOVE
-#
-#####
+    def mark(self) -> int:
+        score = 0
+        for question in self._quiz.questions:
+            if question.chosen_answer == question.correct_answer:
+                score += 1
+        if score == 0:
+            return 0
+        return (score / len(self._quiz.questions) * 100)
+
+    def generate_assessment(self) -> Assessment:
+        if self._quiz.type == allowed_types[0]:
+            assessment = MultipleChoiceAssessment(self._quiz.name)
+        if self._quiz.type == allowed_types[1]:
+            assessment = TechnicalAssessment(self._quiz.name)
+        if self._quiz.type == allowed_types[2]:
+            assessment = PresentationAssessment(self._quiz.name)
+        return assessment
+
+
+class Trainee():
+    def __init__(self, name: str, email: str, date_of_birth: date, assessments: list[Assessment] = []):
+        self.name = name
+        if "@" not in email or ".com" not in email:
+            raise TypeError("Please enter a valid email.")
+        self.email = email
+        self.date_of_birth = date_of_birth
+        for assessment in assessments:
+            if not isinstance(assessment, Assessment):
+                raise TypeError(
+                    "All assessments must be subclass of Assessment.")
+        self.assessments = []
+
+    def get_age(self) -> int:
+        today = date.today()
+        age = int(today.year) - self.date_of_birth.year - \
+            ((int(today.month), int(today.day)) <
+             (self.date_of_birth.month, self.date_of_birth.day))
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        if not isinstance(assessment, Assessment):
+            raise TypeError("Must be Assessment object")
+        self.assessments.append(assessment)
+        return None
+
+    def get_assessment(self, name: str) -> None:
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+        return None
+
+    def get_assessment_of_type(self, type: str) -> list[Assessment]:
+        assessment_list = []
+        if type not in allowed_types:
+            raise ValueError("Assessment type not found.")
+        for assessment in self.assessments:
+            if assessment.type == type:
+                assessment_list.append(assessment)
+        return assessment_list
+
+
+class Assessment():
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        if type in allowed_types:
+            self.type = type
+        else:
+            raise ValueError("Type must be in allowed types.")
+        if score >= 0 and score <= 100:
+            self.score = score
+        else:
+            raise ValueError("Score must be between 0-100")
+
+
+class MultipleChoiceAssessment(Assessment):
+    def __init__(self, name: str, score: float = 100):
+        self.score = score
+        self.name = name
+        self.type = allowed_types[0]
+
+    def calculate_score(self) -> int:
+        return self.score * 0.7
+
+
+class TechnicalAssessment(Assessment):
+    def __init__(self, name: str, score: float = 1000):
+        self.score = score
+        self.name = name
+        self.type = allowed_types[1]
+
+    def calculate_score(self) -> int:
+        return self.score
+
+
+class PresentationAssessment(Assessment):
+    def __init__(self, name: str, score: float = 100):
+        self.score = score
+        self.name = name
+        self.type = allowed_types[2]
+
+    def calculate_score(self) -> int:
+        return self.score * 0.6
 
 
 class Question:
@@ -28,12 +130,6 @@ class Quiz:
         self.questions = questions
         self.name = name
         self.type = type
-
-
-class Marking:
-
-    def __init__(self, quiz: Quiz) -> None:
-        pass
 
 
 if __name__ == "__main__":
